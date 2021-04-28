105bddc63590dce5a88b9f50b51744d28bd72b18
Erick Silva <erick.silva@firstdecision.com.br>
2021-04-28 11:18:18 -0300

    using the constructor

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,27 +1,32 @@
 class Product {
   title;
   imageUrl;
-  description;
   price;
+  description;
+
+  constructor(title, image, price, desc) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
 }
 
 const productsList = {
   products: [
-    {
-      title: 'Product A',
-      imageUrl:
-        'https://i.pinimg.com/originals/71/66/10/716610c301922076632c53b87a54fa96.jpg',
-      price: 599.95,
-      description: 'A civilized weapon',
-    },
+    new Product(
+      'Product A',
+      'https://i.pinimg.com/originals/71/66/10/716610c301922076632c53b87a54fa96.jpg',
+      599.95,
+      'A civilized weapon'
+    ),
 
-    {
-      title: 'Product B',
-      imageUrl:
-        'https://i.pinimg.com/originals/c5/09/73/c50973f1e4c41562c1b0d955fbeb80ef.jpg',
-      price: 999.95,
-      description: 'So uncivilized',
-    },
+    new Product(
+      'Product B',
+      'https://i.pinimg.com/originals/c5/09/73/c50973f1e4c41562c1b0d955fbeb80ef.jpg',
+      999.95,
+      'So uncivilized'
+    ),
   ],
   render() {
     const listHook = document.getElementById('app');
